0932de4f9357514365620ed815d6cf82ae588038
ravi-konjeti <71940212+ravi-konjeti@users.noreply.github.com>
2023-06-05 20:47:59 -0500

    Day 27: Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ and make it into a proper readable format and sends the
 news through email
 """
 
+topic = "tesla"
+
 api_key = "11a66b6e97a84a61b3688f877f8f8704"
 # By using this we saved all the json data into url variable
 # where now we can make use of that data
-url = "https://newsapi.org/v2/everything?q=tesl" \
-      "a&sortBy=publishedAt&apiKey=11a66b6e97a84a6" \
-      "1b3688f877f8f8704"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=11a66b6e97a84a61b3688f877f8f8704&" \
+      "language=en"
 
 request = requests.get(url)
 
@@ -32,8 +36,13 @@ content = request.json()
 body = ""
 # Accessed the article titles and description
 for article in content["articles"]:
-      body = body + article["title"] + "\n" + article["description"] + 2*"\n"
 
+      body ="Subject: Today's news" + "\n" +\
+            body + article["title"] + "\n" +\
+            article["description"] + "\n"+ article["url"] + 2 * "\n"
+
+# we had to change the string data to utf-8 because we were getting an ascii error
+# so we changed the encoding of data
 body = body.encode('utf-8')
 se.send_email(body)
 print("Email_sent")
